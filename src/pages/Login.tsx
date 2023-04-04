1f95fff5d5356fa33ad92474bf3a1b0e598edc93
nachorsanz <nachorsanz@gmail.com>
2023-04-04 09:40:33 +0200

    chore: mock hooks test

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Login from '../ui/features/login-component/login-component';
 import { createUser } from '../api/create-user/create-user';
 import styled from '@emotion/styled';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPageContainer = styled.div`
   display: flex;
@@ -11,9 +12,13 @@ const LoginPageContainer = styled.div`
 `;
 
 const LoginPage = () => {
+    const navigate = useNavigate()
   const onLogin = (email: string, password: string) => {
     const loggedUser = createUser({ email, password });
-    console.log(loggedUser);
+    loggedUser.then((user) => {
+        if (user.user) navigate('/home')
+
+      });
   };
 
   return (
--- src/pages/login.ui-jest.spec.tsx
+++ src/pages/login.ui-jest.spec.tsx
@@ -5,6 +5,12 @@ import '@testing-library/jest-dom/extend-expect';
 import Login from './Login';
 import { render } from '@testing-library/react';
 
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: jest.fn(),
+  }));
+
 describe('Login', () => {
   it('should render', () => {
     const { getByTestId } = render(<Login />);
